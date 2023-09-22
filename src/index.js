a874a7c439186d88466f1908ee50c291ac39a0aa
archananair14 <archana.dineshnair@gmail.com>
2023-09-22 15:59:03 +0100

    Added icons and changed description

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
@@ -10,9 +9,17 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let key = "908252f0ce90156474cc02b551639303";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}&units=metric`;
+let city = "London";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+
 axios.get(url).then(displayTemperature);
 
 let now = new Date();
