17a9d4d8e366cb1245fa5dc633f744737b07bff6
archananair14 <archana.dineshnair@gmail.com>
2023-10-04 13:37:44 +0100

    Made an API call with lat and long (open weather)

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ let days = [
 let day = days[now.getDay()];
 dateElement.innerHTML = `${day} ${hours}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thur", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  let days = ["Thur", "Fri", "Sat"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -32,6 +32,13 @@ alt=""/>  <div class="weather-temperatures"> <span class="weather-temp-max"> 18Â
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let key = "908252f0ce90156474cc02b551639303";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}}&appid=${key}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -52,6 +59,8 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
